ed402416b94817acd7591748f0084cfbd06c9041
Yury Vinogradov <41233522+yuvinogradov@users.noreply.github.com>
2022-05-29 14:40:58 +0300

    refactor: remove tag selectors according to rule 3.4 in html-and-css.html

--- app.js
+++ app.js
@@ -10,21 +10,24 @@
 
 var taskInput = document.getElementById("new-task");//Add a new task.
 var addButton = document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder = document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder = document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder = document.getElementById("completed-tasks");//completed-tasks
 
 
 //New task list item
 var createNewTaskElement = function (taskString) {
 
-  var listItem = document.createElement("li");
-
+  var listItem = document.createElement("li")
+  listItem.classList.add("task-list-item");
   //input (checkbox)
   var checkBox = document.createElement("input");//checkbx
+  checkBox.classList.add("task-list-input-cb")
   //label
   var label = document.createElement("label");//label
+  label.classList.add("task", "task-list-label")
   //input (text)
   var editInput = document.createElement("input");//text
+  editInput.classList.add("task", "text-input", "task-list-input-text")
   //button.edit
   var editButton = document.createElement("button");//edit button
 
@@ -33,18 +36,19 @@ var createNewTaskElement = function (taskString) {
   var deleteButtonImg = document.createElement("img");//delete button image
 
   label.innerText = taskString;
-  label.className = 'task';
+  label.className = "task";
 
   //Each elements, needs appending
   checkBox.type = "checkbox";
   editInput.type = "text";
-  editInput.className = "task";
+  // editInput.className = "task";
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
   editButton.className = "edit";
 
   deleteButton.className = "delete";
-  deleteButtonImg.src = './remove.svg';
+  deleteButtonImg.src = "./remove.svg";
+  deleteButtonImg.className = "button-del-img"
   deleteButton.appendChild(deleteButtonImg);
 
 
@@ -82,7 +86,7 @@ var editTask = function () {
 
   var listItem = this.parentNode;
 
-  var editInput = listItem.querySelector('input[type=text]');
+  var editInput = listItem.querySelector("input[type=text]");
   var label = listItem.querySelector("label");
   var editBtn = listItem.querySelector(".edit");
   var containsClass = listItem.classList.contains("edit-mode");
